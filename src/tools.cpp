869573778424b44cd63c955a21cf6a9219a8e951
jacquestkirk <jacquestkirk@gmail.com>
2018-02-03 21:48:21 -0600

    Additional changes to convert get it to build in VS

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,4 +15,48 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+
+	VectorXd rmse = VectorXd(4);
+	rmse << 0, 0, 0, 0;
+
+	if (estimations.size() != ground_truth.size())
+	{
+		cout << "Invalid estimation or ground_truth data" << endl;
+		return rmse;
+	}
+	if (estimations.size() == 0)
+	{
+		cout << "Invalid estimation or ground_truth data" << endl;
+		return rmse;
+	}
+
+
+
+	//accumulate squared residuals
+
+	VectorXd sum_residuals_squared(4);
+	sum_residuals_squared << 0, 0, 0, 0;
+
+	for (int i = 0; i < estimations.size(); ++i) {
+		// ... your code here
+
+		VectorXd diff = estimations[i] - ground_truth[i];
+
+
+		VectorXd residuals_squared = diff.array() * diff.array();
+
+		sum_residuals_squared += residuals_squared;
+
+	}
+
+	//calculate the mean
+	// ... your code here
+	VectorXd mean = sum_residuals_squared / estimations.size();
+
+	//calculate the squared root
+	// ... your code here
+	rmse = mean.array().sqrt();
+
+	//return the result
+	return rmse;
 }
